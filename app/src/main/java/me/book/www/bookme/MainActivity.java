0f6fcbc9d1ea672f79a3275cba5ac3e88a11dcd9
James_Desktop <james.cotellesse@gmail.com>
2020-09-18 17:10:46 +0900

    Hello World, making sure it all works for now

--- app/src/main/java/me/book/www/bookme/MainActivity.java
+++ app/src/main/java/me/book/www/bookme/MainActivity.java
@@ -3,12 +3,17 @@ package me.book.www.bookme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.main_text_view);
+        textView.setText(R.string.hello_world);
     }
 }
